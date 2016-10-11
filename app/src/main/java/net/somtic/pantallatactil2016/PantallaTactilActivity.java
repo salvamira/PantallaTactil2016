dfc63ea37df9b0fd93e8759cd2fc5744a71c182a
Salva Mira <smira@iesperemaria.com>
2016-10-11 09:01:30 +0200

    Exercici 5.9 (multitouch)

--- app/src/main/java/net/somtic/pantallatactil2016/PantallaTactilActivity.java
+++ app/src/main/java/net/somtic/pantallatactil2016/PantallaTactilActivity.java
@@ -22,8 +22,22 @@ public class PantallaTactilActivity extends AppCompatActivity implements View.On
         TextView salida = (TextView) findViewById(R.id.TextViewSalida);
         float presion = motionEvent.getPressure();
         float tam = motionEvent.getSize();
-        salida.append("\nPresión: " + presion + "\n" + "Tamaño: " + tam + "\n");
-        salida.append("\n" + motionEvent.toString()+"\n" );
+        //salida.append("\nPresión: " + presion + "\n" + "Tamaño: " + tam + "\n");
+        //salida.append("\n" + motionEvent.toString()+"\n" );
+        String acciones[] = { "ACTION_DOWN", "ACTION_UP", "ACTION_MOVE",
+                "ACTION_CANCEL", "ACTION_OUTSIDE", "ACTION_POINTER_DOWN",
+                "ACTION_POINTER_UP" };
+        int accion = motionEvent.getAction();
+        int codigoAccion = accion & MotionEvent.ACTION_MASK;
+        int iPuntero = (accion & MotionEvent.ACTION_POINTER_INDEX_MASK)
+                >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+        salida.append("\n" + acciones[codigoAccion]);
+        salida.append(" Generada por puntero: " + iPuntero);
+        for (int i = 0; i < motionEvent.getPointerCount(); i++) {
+            salida.append("\npuntero:" + motionEvent.getPointerId(i) +
+                    " x:" + motionEvent.getX(i) + " y:" + motionEvent.getY(i));
+        }
+        salida.append("\n");
         return true;
     }
 }
